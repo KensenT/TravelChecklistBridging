6120c2da486a2e747eb37d2fa252ac35a7263176
Resky Javieri <reskyjavier@gmail.com>
2018-10-24 18:59:05 +0700

    Onboarding

---
+++ MC1/MC1/Onboarding/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  MC1
+//
+//  Created by Resky Javieri on 24/10/18.
+//  Copyright © 2018 Ferlix Yanto Wang. All rights reserved.
+//
+
+import UIKit
+
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
---
+++ MC1/MC1/Onboarding/ThirdViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.swift
+//  Onboarding
+//
+//  Created by Resky Javieri on 24/10/18.
+//  Copyright © 2018 Resky Javieri. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "Onboarding3", ofType: "mp4")
+        playVideo(url: URL(fileURLWithPath: path!))
+    }
+    
+    func playVideo(url: URL) {
+        
+        let player = AVPlayer(url: url)
+        let layer: AVPlayerLayer = AVPlayerLayer(player: player)
+        layer.backgroundColor = UIColor.white.cgColor
+        layer.frame = CGRect(x: 75, y: 260, width: 240, height: 448)
+        layer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(layer)
+        player.actionAtItemEnd = .none
+        
+        
+        player.play()
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { (_) in
+            player.seek(to: kCMTimeZero)
+            player.play()
+        }
+        
+    }
+    
+}
